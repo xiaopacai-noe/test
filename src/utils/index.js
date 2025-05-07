e0170d8ee7b2ec44027acd045911fd82c7986ba2
xianyi <2870005947@qq.com>
2025-05-07 10:12:49 +0800

    1

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,7 +7,7 @@ const getSourceMap = async (url) => {
 const findCodeBySourceMap = async (stackFrame) => {
   console.log("%c [ stackFrame ]-7", "font-size:13px; background:pink; color:#bf2c9f;", stackFrame);
   //获取map文件
-  const fileContent = await getSourceMap(stackFrame.filename + ".map");
+  const fileContent = await getSourceMap(stackFrame.fileName + ".map");
   //解析map文件
   const consumer = await new sourceMap.SourceMapConsumer(fileContent);
   //获取源码
